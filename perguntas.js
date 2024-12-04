01125e26314b0810ef970b588f04f84c0d8e0290
loira017 <00001105905597sp@al.educacao.sp.gov.br>
2024-12-04 13:23:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'filme',
+    'a culpa é das?',
+    'Estrelas'
+)
+
+criaCartao(
+    'Desenho',
+    'Desenho em que o mundo é do?',
+    'greg'
+)
+
+criaCartao(
+    'serie',
+    'Qual é a serie em que eles se perem no espaço?',
+    'Perdidos no espaçob n n'
+)
+
+criaCartao(
+    'livro',
+    'tem na biblioteca do brandâo, sobre espaço e tem uma capa preta bonita',
+    '2001: uma odisseia no espaço'
+)
